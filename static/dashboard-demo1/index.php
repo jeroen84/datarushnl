26b7cc571b65a9fb1819d5b373ce75a1fb234003
Jeroen van de Erve <jeroenvde@gmail.com>
2020-03-05 15:45:50 +0100

    final commit to datarush.nl

---
+++ static/dashboard-demo1/index.php
@@ -0,0 +1,2 @@
+<?php
+    Header("Location: https://datarush-demo1.azurewebsites.net/", true, 301);
---
+++ static/datasets/index.php
@@ -0,0 +1,2 @@
+<?php
+    Header("Location: https://www.linkedin.com/in/jeroen-van-de-erve/", true, 301);
